0fcd96be83378a91734eead7f9a3ca4ab042defc
Zirwa Abid <zirwaabid112@gmail.com>
2025-02-19 12:45:12 +0500

    qalam

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.use(express.urlencoded({ extended: true }));
 const path = require("path");
 app.set("view engine", "ejs")
 
+// require and setting for ejs mate
+const ejsmate = require('ejs-mate');
+app.engine('ejs', ejsmate);
+
 //for require views,public folder in every file use following code
 app.set("views", path.join(__dirname, "/views"))
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,6 +21,9 @@ app.use(methodOverride("_method"))
 
 // require all models 
 const Unstitched = require("./models/Unstitched.js");
+const Stitched = require("./models/stitched.js");
+const Fragnances = require("./models/fragnances.js");
+const Beauty = require("./models/beauty.js");
 
 // require and setting for mongoose 
 const mongoose = require("mongoose");
@@ -31,13 +38,30 @@ async function main() {
 };
 
 // home page
-app.get("/qalam", async(req, res) => {
+app.get("/qalam", async (req, res) => {
     res.render("listings/index.ejs");
 })
 // Unstitched page
-app.get("/qalam/unstitched",async(req,res)=>{
+app.get("/qalam/unstitched", async (req, res) => {
     const allUnstitched = await Unstitched.find({});
     res.render("listings/unstitched.ejs", { allUnstitched });
+})
+// stitched page
+app.get("/qalam/stitched", async (req, res) => {
+    const allstitched = await Stitched.find({});
+    res.render("listings/stitched.ejs", { allstitched });
+})
+
+// Fragnances page
+app.get("/qalam/fragnances", async (req, res) => {
+    const allFragnances = await Fragnances.find({});
+    res.render("listings/fragnance.ejs", { allFragnances });
+})
+
+// beauty page
+app.get("/qalam/beauty", async (req, res) => {
+    const allBeauty = await Beauty.find({});
+    res.render("listings/beauty.ejs", { allBeauty });
 })
 
 app.listen("8080", () => {
